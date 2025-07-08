5109e733602055d481de129868d5be29f2b0926b
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-07-08 22:01:42 +0530

    testing -/-\-/-\-****

--- apps/www/app/(public)/auth/page.tsx
+++ apps/www/app/(public)/auth/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { LoginForm } from "@/components/login-form";
-import { useAuth } from "@/context/auth.context";
-import { redirect } from "next/navigation";
+// import { useAuth } from "@/context/auth.context";
+// import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-  const { user } = useAuth();
-  if (user) {
-    redirect("/dashboard");
-  }
+  // const { user } = useAuth();
+  // if (user) {
+  //   redirect("/dashboard");
+  // }
 
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
