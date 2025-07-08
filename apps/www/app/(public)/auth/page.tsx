a3cbac5dab3878622a431a2eed371c2601315f23
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-07-08 22:06:27 +0530

    testing -/-\-/-\-****eplepple

--- apps/www/app/(public)/auth/page.tsx
+++ apps/www/app/(public)/auth/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoginForm } from "@/components/login-form";
+// import { LoginForm } from "@/components/login-form";
 // import { useAuth } from "@/context/auth.context";
 // import { redirect } from "next/navigation";
 
@@ -13,7 +13,8 @@ export default function LoginPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        {/* <LoginForm /> */}
+        fuck off
       </div>
     </div>
   );
