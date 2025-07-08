635cd7ebeccf620ea8af9551d92949b4c0dfb416
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-07-08 21:09:03 +0530

    testing ---

--- apps/www/app/(public)/auth/page.tsx
+++ apps/www/app/(public)/auth/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { LoginForm } from "@/components/login-form";
-import { useAuth } from "@/context/auth.context";
+import { useMyProfileStore } from "@prexo/store";
 import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-  const { user } = useAuth();
-  if (user) {
+  const { myProfile } = useMyProfileStore();
+  if (myProfile) {
     redirect("/dashboard");
   }
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
