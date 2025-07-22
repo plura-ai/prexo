da7489c8556cd001f545c9eea324955ef5490841
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-07-22 21:29:13 +0530

    fix auth page 004

--- apps/www/app/(public)/auth/page.tsx
+++ apps/www/app/(public)/auth/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { LoginForm } from "@/components/custom/login-form";
-import { useMyProfileStore } from "@prexo/store";
-import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-  const { myProfile } = useMyProfileStore();
-  if (myProfile) {
-    redirect("/dashboard");
-  }
 
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
