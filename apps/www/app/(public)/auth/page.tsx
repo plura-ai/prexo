e7c909ae2aeb87f59c1e2465d504a8d180047f97
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-07-22 21:21:12 +0530

    fix auth page 002

--- apps/www/app/(public)/auth/page.tsx
+++ apps/www/app/(public)/auth/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
 import { LoginForm } from "@/components/custom/login-form";
-import { useAuth } from "@/context/auth.context";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useMyProfileStore } from "@prexo/store";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-  const { user } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      router.replace("/dashboard");
-    }
-  }, [user, router]);
-
-  if (user) {
-    return null;
+  const { myProfile } = useMyProfileStore();
+  if (myProfile) {
+    redirect("/dashboard");
   }
 
   return (
